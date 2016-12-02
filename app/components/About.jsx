924c45ce60e549ee6a4508f9eea08ae8c0b54105
vm95604b <vm95604b@gmail.com>
2016-12-02 10:23:05 -0500

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -16,8 +16,22 @@ var React = require('react');
 var About = (props) => {
 		return (
 			<div>
-			<h3>About</h3>
-			<p>Welcome to the About Page</p>
+
+				<h1 className='text-center'>About</h1>
+				
+				<p>This is a weather application built with React.</p>
+
+				<p>Here are some of the tools I used</p>
+
+				<ul>
+					<li>
+						<a href='https://facebook.github.io/react'>React</a> - JavaScript framework used.
+					</li>
+					<li>
+						<a href='https://openweathermap.org'>Open Weather Map</a> - Open Weather Map used to search for weather data by city name.
+					</li>
+				</ul>
+
 			</div>
 		);
 };
