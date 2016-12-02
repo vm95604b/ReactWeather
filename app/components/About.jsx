71ef98c7e5ec93ee37d1699ce48559bbb0eaeeba
vm95604b <vm95604b@gmail.com>
2016-12-02 09:07:25 -0500

    challange question commit

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,7 +15,10 @@ var React = require('react');
 // Only on functions that don't maintain any state.
 var About = (props) => {
 		return (
-			<h3>About Comp</h3>
+			<div>
+			<h3>About</h3>
+			<p>Welcome to the About Page</p>
+			</div>
 		);
 };
 
