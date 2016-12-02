ccf9fd91e81b44e20aac1e451473f379d6c35c80
vm95604b <vm95604b@gmail.com>
2016-12-02 09:54:27 -0500

    Style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,37 +2,37 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 
-// Refactored with arrow functions
-// var Nav = React.createClass({
-// 	render: function() {
-// 		return (
-// 			<div>
-// 			<h2>Nav Comp</h2>
-
-// 			<IndexLink to='/' activeClassName='active' activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-			
-// 			<IndexLink to='/about' activeClassName='active' activeStyle={{fontWeight:'bold'}}>About</IndexLink>
-			
-// 			<IndexLink to='/examples' activeClassName='active' activeStyle={{fontWeight:'bold'}}>Examples</IndexLink>
-			
-// 			</div>
-// 		);
-// 	}
-// });
-
-var Nav = () => {
-	return (
-			<div>
-			<h2>Nav Comp</h2>
-
-			<IndexLink to='/' activeClassName='active' activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-			
-			<IndexLink to='/about' activeClassName='active' activeStyle={{fontWeight:'bold'}}>About</IndexLink>
-			
-			<IndexLink to='/examples' activeClassName='active' activeStyle={{fontWeight:'bold'}}>Examples</IndexLink>
-			
+var Nav = React.createClass({
+	onSearch: function(e){
+		e.preventDefault();
+		alert('Not yet wired up');
+	},
+	render: function() {
+		return (
+			//Call HTML classes "className" to remove conflics with jsx
+			<div className='top-bar'>
+				<div className='top-bar-left'>
+					<ul className='menu'>
+					<li className='menu-text'>React Weather App</li>
+					<li><IndexLink to='/' activeClassName='active' activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink></li>
+					<li><IndexLink to='/about' activeClassName='active' activeStyle={{fontWeight:'bold'}}>About</IndexLink></li>
+					<li><IndexLink to='/examples' activeClassName='active' activeStyle={{fontWeight:'bold'}}>Examples</IndexLink></li>
+					</ul>
+				</div>
+				<div className='top-bar-right'>
+					<form onSubmit={this.onSearch}>
+						<ul className='menu'>
+							<li>
+								<input type='search' placeholder='Search Weather'/>
+							</li>
+							<li>
+								<input type='submit' className='button' value='Get Weather'/>
+							</li>
+						</ul>
+					</form>
+				</div>
 			</div>
 		);
-};
-
+	}
+});
 module.exports = Nav;
