699bb831308c9941fffa7d61cc103a5a99b1cd80
vm95604b <vm95604b@gmail.com>
2016-12-03 03:17:11 -0500

    Add custom styles support

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -16,7 +16,7 @@ var WeatherForm = React.createClass({
 		return (
 			<div>
 				<form onSubmit={this.onFormSubmit}>
-					<input type='text' ref='location' placeholder='Enter City...'/>
+					<input type='search' ref='location' placeholder='Enter City...'/>
 					<button className='button expanded hollow'>Get Weather</button>
 				</form>
 			</div>
